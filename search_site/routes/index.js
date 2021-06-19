5e20bb846f0ed6840d364e19e0da3cd0f51df00f
ChiachenCheng <59192936+ChiachenCheng@users.noreply.github.com>
2021-06-19 15:39:37 +0800

    newsshow中内容，已废弃
    本来打算基于这个继续做2的大作业的，后来发现更好的模版，就不用了。

--- search_site/routes/index.js
+++ search_site/routes/index.js
@@ -6,21 +6,31 @@ var fs = require("fs");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-    res.render('index', { title: 'Express' });
+    // res.render('index', { title: 'Express' });
+    res.end(fs.readFileSync("./html/index.html"));
 });
 
 router.get('/process_get', function(request, response) {
     var or_array = [];
-    if(request.query.title!="")
+    var lg_array = [];
+    if(request.query.title!=""){
         or_array.push({"title":{"$regex":request.query.title}})
-    if(request.query.keywords!="")
+        lg_array.push({"title":request.query.title})
+    }
+    if(request.query.keywords!=""){
         or_array.push({"keywords":{"$regex":request.query.keywords}})
-    if(request.query.content!="")
+        lg_array.push({"keywords":request.query.keywords})
+    }
+    if(request.query.content!=""){
         or_array.push({"content":{"$regex":request.query.content}})
-    if(request.query.all!="")
+        lg_array.push({"content":request.query.content})
+    }
+    if(request.query.all!=""){
         or_array=[{"title":{"$regex":request.query.all}}, {"keywords":{"$regex":request.query.all}}, 
                 {"content":{"$regex":request.query.all}}, {"desc":{"$regex":request.query.all}}, 
-                {"source_name":{"$regex":request.query.all}}, {"publish_date":{"$regex":request.query.all}}]
+                {"source_name":{"$regex":request.query.all}}, {"publish_date":{"$regex":request.query.all}}];
+        lg_array.push({"all":request.query.all})
+    }
     var que = {"$or":or_array};
     var col = {"_id":0, "source_encoding":0, "crawltime":0};
     var seq = {"publish_date": -1};
@@ -31,11 +41,13 @@ router.get('/process_get', function(request, response) {
         });
         response.write(JSON.stringify(result));
         response.end();
+        var lg = {"user":request.query.usrname,"search_words":lg_array,"op":"search","state":"SUCCESS","time":Date.now()}
+        mongo.insert_logs(lg)
     });
 });
 
 router.get('/time_analysis', function(request, response) {
-    let $ = cheerio.load(fs.readFileSync("public/time.html"));
+    let $ = cheerio.load(fs.readFileSync("html/time.html"));
     $("h1#kw").replaceWith(`<h1 id="kw">关键词 ${request.query.word} 的时间热度分析</h1>`);
     if(request.query.word==""){
         // response.write(JSON.stringify("no value"));
@@ -69,6 +81,74 @@ router.get('/time_analysis', function(request, response) {
             );
         });
     }
+});
+
+router.post('/register_page', function(request, response) {
+    response.end(fs.readFileSync("./html/register.html"));
+});
+
+router.post('/register', function(request, response){
+    var que = {"name":request.query.usrname};
+    var col = {};
+    var seq = {};
+    mongo.search_user(que, col, seq, function(result){
+        if(result.length == 0){
+            var injson = {"name":request.query.usrname, "passwd":request.query.passwd, "available":1, };
+            mongo.insert_user(injson);
+            var lg = {"user":request.query.usrname,"passwd":request.query.passwd,"op":"sign_up","state":"SUCCESS","time":Date.now()}
+            mongo.insert_logs(lg)
+            response.writeHead(200, {"Content-Type": "application/json"});
+            response.write(JSON.stringify("注册成功！"));
+            response.end();
+        }
+        else{
+            var lg = {"user":request.query.usrname,"passwd":request.query.passwd,"op":"sign_up","state":"FAILURE","time":Date.now()}
+            mongo.insert_logs(lg)
+            response.writeHead(200, {"Content-Type": "application/json"});
+            response.write(JSON.stringify("注册失败！请更换用户名！"));
+            response.end();
+        }
+    });
+});
+
+router.post('/log_in', function(request, response){
+    var que = {"name":request.query.usr};
+    var col = {};
+    var seq = {};
+    response.writeHead(200, {"Content-Type": "application/json"});
+    mongo.search_user(que, col, seq, function(result){
+        if(result.length == 1){
+            var user = result[0];
+            if (request.query.pw == user["passwd"]){
+                if (user["available"] == 1){
+                    response.write(JSON.stringify("TRUE"));
+                    response.end();
+                    var lg = {"user":request.query.usr,"op":"log_in","state":"SUCCESS","time":Date.now()}
+                    mongo.insert_logs(lg)
+                } else {
+                    response.write(JSON.stringify("登陆失败！您的账号已被停用！")); 
+                    response.end();
+                    var lg = {"user":request.query.usr,"op":"log_in","state":"FAILURE","time":Date.now()}
+                    mongo.insert_logs(lg)
+                }            
+            } else {
+                response.write(JSON.stringify("登陆失败！请检查您的密码！")); 
+                response.end();
+                var lg = {"user":request.query.usr,"op":"log_in","state":"FAILURE","time":Date.now()}
+                mongo.insert_logs(lg)
+            }
+        } else {
+            response.write(JSON.stringify("登陆失败！请检查您的用户名！"));
+            response.end();
+        }
+    });
+});
+
+router.get('/search_page', function(request, response) {
+    let user = request.query.user;
+    let $ = cheerio.load(fs.readFileSync("./html/search.html"));
+    $("p#user").text(user);
+    response.end($.html());
 });
 
 module.exports = router;
--- search_site/routes/users.js
+++ search_site/routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require("../mongodb.js");
+var cheerio = require("cheerio");
+var fs = require("fs");
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.end(fs.readFileSync("./html/user.html"));
+});
+
+router.post('/log_in', function(request, response){
+  var que = {"name":request.query.usr, "manage":1};
+  var col = {};
+  var seq = {};
+  response.writeHead(200, {"Content-Type": "application/json"});
+  mongo.search_user(que, col, seq, function(result){
+      if(result.length == 1){
+          var user = result[0];
+          if (request.query.pw == user["passwd"]){
+              response.write(JSON.stringify("TRUE"));
+              response.end();
+              var lg = {"user":request.query.usr,"op":"manage_log_in","state":"SUCCESS","time":Date.now()}
+              mongo.insert_logs(lg)
+          } else {
+              response.write(JSON.stringify("FALSE")); 
+              response.end();
+              var lg = {"user":request.query.usr,"op":"manage_log_in","state":"FAILURE","time":Date.now()}
+              mongo.insert_logs(lg)
+          }
+      } else {
+          var lg = {"user":request.query.usr,"op":"manage_log_in","state":"FAILURE","time":Date.now()}
+          mongo.insert_logs(lg)
+          response.write(JSON.stringify("FALSE"));
+          response.end();
+      }
+  });
 });
 
 module.exports = router;
